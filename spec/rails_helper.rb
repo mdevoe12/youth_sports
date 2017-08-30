f710661eb75e936596c07a3fce1c7857ba0d1554
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 15:37:38 -0600

    added needed end in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,9 +16,10 @@ VCR.configure do |config|
 end
 # Add additional requires below this line. Rails is not loaded until this point!
 Shoulda::Matchers.configure do |config|
-config.integrate do |with|
-  with.test_framework :rspec
-  with.library :rails
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+    end
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
