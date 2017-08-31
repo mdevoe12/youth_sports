e8a83d84e98c57ab3f07cf2d8fc6a687f2a52988
frustratedswede <andrew.mossefin@gmail.com>
2017-08-31 15:57:33 -0600

    fix bux
    repalced quotes in omniauth.rb that were causing errors.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, Rails.application.secrets.ENV['FACEBOOK_APP_ID'],
- Rails.application.secrets.ENV['FACEBOOK_APP_SECRET'], scope: ‘public_profile’, info_fields: ‘id,name,link’
+ provider :facebook, Rails.application.secrets.facebook_app_id,
+ Rails.application.secrets.facebook_app_secret, scope: 'public_profile', info_fields: 'id,name,link'
 end
