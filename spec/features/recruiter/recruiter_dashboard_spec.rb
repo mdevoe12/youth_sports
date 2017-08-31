172163580c725dbbc41e720b82b1b8297cc6db73
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-31 12:53:31 -0600

    remove pending tests and setup recruiter dashboard feature test

---
+++ spec/features/recruiter/recruiter_dashboard_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Registered recruiter logs in' do
+  scenario 'and sees their dashboard with their prospects' do
+    recruiter = Recruiter.create()
+  end
+end
