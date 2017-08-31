03d8fc1b563b1cbd1b34d591a46f373843bd2082
frustratedswede <andrew.mossefin@gmail.com>
2017-08-31 17:31:14 -0600

    add models
    added a conversation model and personal_message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 
   has_many :user_roles
   has_many :roles, through: :user_roles
+  has_many :authored_conversations, class_name: 'Conversation',
+  foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation',
+  foreign_key: 'receiver_id'
 
   self.inheritance_column = :type
 
