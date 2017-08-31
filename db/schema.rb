32c15a8db0870125128450c14357b4224bc5994a
frustratedswede <andrew.mossefin@gmail.com>
2017-08-31 11:30:04 -0600

    add bcrypt
    now passing rspec test with bcrypt enabled

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170831031543) do
-
+ActiveRecord::Schema.define(version: 20170831143740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "admin_profiles", force: :cascade do |t|
     t.string "username"
@@ -26,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170831031543) do
     t.bigint "admin_id"
     t.index ["admin_id"], name: "index_admin_profiles_on_admin_id"
   end
-    
+
   create_table "coach_profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -86,7 +83,6 @@ ActiveRecord::Schema.define(version: 20170831031543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   add_foreign_key "admin_profiles", "users", column: "admin_id"
   add_foreign_key "coach_profiles", "users", column: "coach_id"
