d958f7c8431fd6c64cbaaafedba1328b87726173
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-31 21:45:06 -0600

    MVC setup for dashboard index

--- spec/features/recruiter/recruiter_dashboard_spec.rb
+++ spec/features/recruiter/recruiter_dashboard_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Registered recruiter logs in' do
     Prospect.create(player_profile_id: profile2.id, recruiter_profile_id: rec_profile.id)
     Prospect.create(player_profile_id: profile4.id, recruiter_profile_id: rec_profile.id)
     recruiter.prospects
-require "pry"; binding.pry
+# require "pry"; binding.pry
     visit '/dashboard'
 
   end
