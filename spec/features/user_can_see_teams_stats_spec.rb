519577805c919a829a13f25f25e490a285c8cdeb
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-01 06:48:13 -0600

    building out more robust test

--- spec/features/user_can_see_teams_stats_spec.rb
+++ spec/features/user_can_see_teams_stats_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'as an unregistered user when I visit a team stats' do
   scenario 'I can see a teams stats' do
-    coach = create(:user, type: "Coach")
+    coach = create(:user, type: "Coach", first_name: "Bob", last_name: "Bill")
+    players = create_list(:user, 3, type: "Player")
     team = create(:team)
+    Player.all.each { |player| team.players << player }
     TeamCoach.create(team_id: team.id, coach_id: coach.id)
+
 
     visit "teams/#{team.id}/stats"
 
     expect(page).to have_content(team.name)
+    expect(page).to have_content(coach.first_name)
+    expect(page).to have_content(coach.last_name)
+    expect(page).to have_content("Players: 3")
+
   end
 end
