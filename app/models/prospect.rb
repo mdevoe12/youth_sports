baf0f00cf3559991ddf12e312d83a8b2e5768a5e
Jim Szalewski <jim.szalewski@gmail.com>
2017-09-02 15:53:10 -0600

    cleanup view with prospect.name method

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,4 +1,8 @@
 class Prospect < ApplicationRecord
   belongs_to :recruiter_profile
   belongs_to :player_profile
+
+  def name
+    self.player_profile.player.first_name + ' ' + self.player_profile.player.last_name
+  end
 end
