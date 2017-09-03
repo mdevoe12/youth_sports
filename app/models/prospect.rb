5b09eb309d1f53b6f2dfe1cb3527b39056a32db9
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-03 15:13:12 -0600

    added enum for status, added default of 0

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,4 +1,6 @@
 class Prospect < ApplicationRecord
   belongs_to :recruiter_profile
   belongs_to :player_profile
+
+  enum status: %w(open in-progress denied prospect)
 end
--- db/migrate/20170903210756_add_status_to_prospects.rb
+++ db/migrate/20170903210756_add_status_to_prospects.rb
@@ -1,5 +1,5 @@
 class AddStatusToProspects < ActiveRecord::Migration[5.1]
   def change
-    add_column :prospects, :status, :integer
+    add_column :prospects, :status, :integer, default: 0
   end
 end
