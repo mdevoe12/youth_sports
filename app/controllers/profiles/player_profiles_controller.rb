a00bab82672dbfef315da967a62fbbc3f5540c24
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-03 11:38:49 -0600

    can send a text message to my phone

--- app/controllers/profiles/player_profiles_controller.rb
+++ app/controllers/profiles/player_profiles_controller.rb
@@ -1,8 +1,8 @@
 class Profiles::PlayerProfilesController < ApplicationController
 
   def show
-    account_sid = ENV['test_twilio_sid']
-    auth_token = ENV['test_twilio_token']
+    account_sid = ENV['live_twilio_sid']
+    auth_token = ENV['live_twilio_token']
 
       # @client = Twilio::REST::Client.new ENV['twilio_sid'], ENV['twilio_test_token']
       @client = Twilio::REST::Client.new account_sid, auth_token
@@ -10,7 +10,7 @@ class Profiles::PlayerProfilesController < ApplicationController
       binding.pry
       @message = @client.messages.create({
         to: '+16073426730',
-        from: "+15005550006",
+        from: "+13157571027",
         body: 'This is sent from your twilio_service.rb file'
         })
 
