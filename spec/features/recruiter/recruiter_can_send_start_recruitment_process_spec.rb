623551f3c23b13fb1c1396f6aac3e87f9c11bfa8
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-04 11:48:47 -0600

    cleaned up test

--- spec/features/recruiter/recruiter_can_send_start_recruitment_process_spec.rb
+++ spec/features/recruiter/recruiter_can_send_start_recruitment_process_spec.rb
@@ -24,30 +24,14 @@ RSpec.feature "recruiter can request to start recruitment process" do
       expect(player.profile.prospects.last.status).to eq("in-progress")
       expect(page).to have_content("You've sent a request to the player's guardian.")
 
-      params = {Body: "Yes", From: "+#{profile.guardian_phone}"}
+
       rack_test_session_wrapper = Capybara.current_session.driver
-      rack_test_session_wrapper.post(receive_text_path, params = params)
-      
+      rack_test_session_wrapper.post(receive_text_path,
+                                     params = {
+                                       Body: "Yes",
+                                       From: "+#{profile.guardian_phone}"})
+
       expect(Player.last.profile.prospects.last.status).to eq("prospect")
     end
   end
-
-  xscenario "players status changes when the guardian response with a yes" do
-      recruiter = User.create(type: "Recruiter",
-                          first_name: "test",
-                          last_name: "test",
-                          password: "123")
-      recr_profile = RecruiterProfile.create(recruiter_id: recruiter.id)
-      player = User.create(type: "Player", password: "123")
-      profile = PlayerProfile.create(player_id: player.id, guardian_phone: "16073426730")
-      PlayerStat.create(points: 40, fouls: 20, player_profile: profile)
-
-      params = {Body: "Yes", From: "+#{profile.guardian_phone}"
-      }
-      binding.pry
-      visit receive_text_path
-
-
-  end
-
 end
