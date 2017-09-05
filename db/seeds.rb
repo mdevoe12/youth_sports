0732e5e31b83798fd5b427e84d97756bb79f8049
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-05 15:56:04 -0600

    building out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Role.create(name: "Player")
+Role.create(name: "Coach")
+Role.create(name: "Recruiter")
+Role.create(name: "Admin")
+
+4.times do
+    team = Team.create(
+    name: Faker::Team.name
+    )
+end
+
+
+10.times do
+  player = Player.create(
+    :first_name => Faker::Name.first_name,
+    :last_name  => Faker::Name.last_name,
+    :username   => Faker::Internet.safe_email,
+    :password   => Faker::Internet.password(8)
+  )
+
+  player.roles << Role.find_by(name: "Player")
+
+  player_profile = PlayerProfile.create(
+    :school => Faker::HarryPotter.house,
+    :height => "#{rand(4...7)} foot #{rand(1...11)} inches",
+    :weight => "#{rand(110...250)}",
+    :grade_level => rand(1..12),
+    :gpa => rand(1.1...4.0).round(2),
+    :guardian_phone => "15555555555",
+    :player_id => player.id
+  )
+
+  PlayerStat.create(
+    :points => rand(1..30),
+    :fouls => rand(0..5),
+    :player_profile_id => player_profile.id
+  )
+
+  TeamPlayer.create(
+    :team_id => Team.pluck(:id).sample,
+    :player_id => player.id
+  )
+end
+
+10.times do
+  coach = Coach.create(
+    :first_name => Faker::Name.first_name,
+    :last_name  => Faker::Name.last_name,
+    :username   => Faker::Internet.safe_email,
+    :password   => Faker::Internet.password(8)
+  )
+
+  coach.roles << Role.find_by(name: "Coach")
+
+  CoachProfile.create(
+    :institution => Faker::GameOfThrones.house,
+    :email => Faker::Internet.safe_email,
+    :phone_number => "1234545456",
+    :coach_id => coach.id
+    )
+
+  TeamCoach.create(
+    :team_id => Team.pluck(:id).sample,
+    :coach_id => coach.id
+  )
+
+    recruiter = Recruiter.create(
+      :first_name => Faker::Name.first_name,
+      :last_name  => Faker::Name.last_name,
+      :username   => Faker::Internet.safe_email,
+      :password   => Faker::Internet.password(8)
+      )
+
+    recruiter.roles << Role.find_by(name: "Recruiter")
+
+    recruiter_profile = RecruiterProfile.create(
+      :institution => Faker::GameOfThrones.house,
+      :email => Faker::Internet.safe_email,
+      :phone_number => "1234545456",
+      :recruiter_id => recruiter.id
+    )
+
+    Prospect.create(
+      :recruiter_profile_id => recruiter_profile.id,
+      :player_profile_id => Player.pluck(:id).sample,
+      :status => rand(0..3)
+    )
+end
