07b9e7e7bcb427b907a7d3b5f1b4a90ee1ab16cf
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-05 20:40:15 -0600

    refactored TwilioService

--- spec/services/twilio_service_spec.rb
+++ spec/services/twilio_service_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TwilioService do
-  
+  context "Twilio connection" do
+    it "returns a valid connection" do
+      VCR.use_cassette("/services/twilio_service_spec.rb") do
+        connection = TwilioService.connect
+
+        expect(connection.username).to eq(ENV['twilio_sid'])
+        expect(connection.auth_token).to eq(ENV['twilio_token'])        
+      end
+    end
+  end
 end
