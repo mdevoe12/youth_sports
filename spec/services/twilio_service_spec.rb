701de127dcea3ebe6310b418bc578996557559d0
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-05 20:51:55 -0600

    service test for sending recruiter test passes

--- spec/services/twilio_service_spec.rb
+++ spec/services/twilio_service_spec.rb
@@ -7,7 +7,18 @@ RSpec.describe TwilioService do
         connection = TwilioService.connect
 
         expect(connection.username).to eq(ENV['twilio_sid'])
-        expect(connection.auth_token).to eq(ENV['twilio_token'])        
+        expect(connection.auth_token).to eq(ENV['twilio_token'])
+      end
+    end
+
+    it "sends a message" do
+      VCR.use_cassette("/services/twilio_service_spec.rb") do
+        message = TwilioMessage.new("16073426730")
+        request = TwilioService.send_recruiter_request(message)
+
+        expect(request.to).to eq(message.to)
+        expect(request.from).to eq(message.from)
+        expect(request.uri).to eq("/2010-04-01/Accounts/#{ENV['twilio_sid']}/Messages/#{request.sid}.json")
       end
     end
   end
