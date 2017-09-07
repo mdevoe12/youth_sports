97154d0014889b388c825af1ef112db80ced8e40
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-07 11:22:45 -0600

    added test for unreged user visiting coach profile

--- spec/features/unregistered/unregistered_user_cannot_view_dashboard_spec.rb
+++ spec/features/unregistered/unregistered_user_cannot_view_dashboard_spec.rb
@@ -10,11 +10,18 @@ RSpec.feature "unregistered user" do
   scenario "cannot view player profiles" do
     player = Player.create(first_name: "steve", last_name: "madden", password: "123")
     profile = PlayerProfile.create(school: "BG", player_id: player.id)
-    
 
     visit player_profile_path(profile.id)
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
 
+  scenario "cannot view coach profile" do
+    coach = Coach.create(first_name: "harry", last_name: "henderson", password: "123")
+    profile = CoachProfile.create(school: "BG", coach_id: coach.id)
+
+    visit coach_profile_path(profile.id)
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")  
   end
 end
