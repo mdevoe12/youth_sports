3c8a4d3c4869b75ea86cfa36e5fcdc75bb8bfa2c
frustratedswede <andrew.mossefin@gmail.com>
2017-09-07 03:44:28 -0600

    auth
    Created functional 'Login' and 'Logout' links in navbar.
    Links are scoped so that you can only see 'Sign Up' or 'Login'
    when user is not logged in. Only 'Logout' link shows when
    user is logged in. Further work needed to implement Twitter auth.
    'Login' form also was created. Need to style navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -17,15 +17,17 @@ Rails.application.routes.draw do
     get '/:id/stats', to: 'stats#index'
   end
 
-  get '/:id/messages', to: 'conversations#index'
-  get '/:id/favorite_player', to: 'favorite_player#create'
-  get '/logout', to: 'sessions#destroy'
+  get  '/:id/messages', to: 'conversations#index'
+  get  '/:id/favorite_player', to: 'favorite_player#create'
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get  '/logout', to: 'sessions#destroy'
   post '/send_text', to: 'twilio#create'
   post '/receive_text', to: 'twilio#update'
 
   resources :players, only: [:new, :create, :update]
   resources :player_profiles, only: [:new, :create, :show]
-  resources :sessions, only: [:create, :destroy]
+  # resources :sessions, only: [:create, :destroy]
   resources :personal_messages, only: [:new, :create]
   resources :conversations, only: [:new, :index, :show]
   resources :athletes, as: :players, :controller => :players, only: [:show, :edit]
