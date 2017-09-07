d6fe52eee7f53052bf445fb6ac5021e66d327bc4
Brandon Randall <br.mhcexchange@gmail.com>
2017-09-06 18:51:30 -0600

    implements the ability to login with twitter or facebook oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,27 @@
 class SessionsController < ApplicationController
 
   def create
-    twitter = request.env['omniauth.auth']
 
-    user = User.find_or_create_from_twitter_auth(twitter)
-      # binding.pry
+    if params[:oauth_token]
+      twitter = request.env['omniauth.auth']
+      player = Player.find_or_create_from_twitter_auth(twitter)
+        player.save
+        session[:user_id] = player.id
+        redirect_to player_dashboard_path(player)
+    else
+      @response = Faraday.get("https://graph.facebook.com/v2.10/oauth/access_token?client_id=#{ENV['FACEBOOK_APP_ID']}&redirect_uri=http://localhost:3000/auth/facebook/callback&client_secret=#{ENV['FACEBOOK_APP_SECRET']}&code=#{params["code"]}")
+      token = @response.body.split(/\W+/)[2]
+      oauth_response = Faraday.get("https://graph.facebook.com/debug_token?input_token=#{token}&access_token=#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}")
+      auth = JSON.parse(oauth_response.body)
+      user = User.find_or_create_by(uid: auth["data"]["user_id"])
+      user.uid = auth["data"]["user_id"]
+      user.provider = "facebook"
+      user.type = "Player"
       user.save
+
       session[:user_id] = user.id
-      redirect_to player_dashboard_path(user)
-      # redirect_to root_path
-    # end
 
-    # @response = Faraday.get("https://graph.facebook.com/v2.10/oauth/access_token?client_id=#{ENV['FACEBOOK_APP_ID']}&redirect_uri=http://localhost:3000/auth/facebook/callback&client_secret=#{ENV['FACEBOOK_APP_SECRET']}&code=#{params["code"]}")
-    # token = @response.body.split(/\W+/)[2]
-    # oauth_response = Faraday.get("https://graph.facebook.com/debug_token?input_token=#{token}&access_token=#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}")
-    # auth = JSON.parse(oauth_response.body)
-    # user = User.find_or_create_by(uid: auth["data"]["user_id"])
-    # user.uid = auth["data"]["user_id"]
-    # user.provider = "facebook"
-    # user.type = "Player"
-    # user.save
-    #
-    # session[:user_id] = user.id
-
-    # redirect_to root_path
-
+      redirect_to root_path
+    end
   end
-
-  private
-
-  def twitter_auth
-    request.env['omniauth.auth']
-  end
-
 end
