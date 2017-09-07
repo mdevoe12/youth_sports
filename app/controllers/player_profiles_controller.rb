339f1ec02aac7aa8bee64bcde1bf0d26b7f5ce93
Jim Szalewski <jim.szalewski@gmail.com>
2017-09-07 13:49:01 -0600

    hit a wall, merging with group

--- app/controllers/player_profiles_controller.rb
+++ app/controllers/player_profiles_controller.rb
@@ -10,11 +10,10 @@ class PlayerProfilesController < ApplicationController
 
   def show
     @player_profile = PlayerProfile.find(params[:id])
-    @player = Player.find(@player_profile.player_id)
     if current_user.type == "Recruiter"
-
       @player = current_user.prospects.find_by(player_profile_id: params[:id]).player_profile.player
     else
+      @player = Player.find(@player_profile.player_id)
     end
   end
 
