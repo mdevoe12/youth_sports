0b370b65f525002f4d622382e25011b718fe1680
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-07 11:19:49 -0600

    added test for unregistered user cannot view player profile

--- spec/features/unregistered/unregistered_user_cannot_view_dashboard_spec.rb
+++ spec/features/unregistered/unregistered_user_cannot_view_dashboard_spec.rb
@@ -5,6 +5,16 @@ RSpec.feature "unregistered user" do
     visit '/dashboard'
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
-    # expect(visit "/dashboard").to raise_error(ActionController::RoutingError)
+  end
+
+  scenario "cannot view player profiles" do
+    player = Player.create(first_name: "steve", last_name: "madden", password: "123")
+    profile = PlayerProfile.create(school: "BG", player_id: player.id)
+    
+
+    visit player_profile_path(profile.id)
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+
   end
 end
