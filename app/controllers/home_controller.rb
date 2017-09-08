617ef6bfb0cac87ac246bb78b70b49aeb6b5ac94
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-09-07 23:07:51 -0600

    deletes commented text in home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,7 @@ class HomeController < ApplicationController
 
 
   def index
-    # tweet_info = build_twitter_service
-    # @user = tweet_info.user
-    # @tweets = tweet_info.find_tweets_by_screen_name("@KingJames")
+    
   end
 
   private
