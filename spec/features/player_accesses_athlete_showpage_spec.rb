be41a786f0317a5f1b7c69e13a53764b2dfb16b2
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-09-07 21:32:12 -0600

    fixes all rspec related issues after mergin profiles together

--- spec/features/player_accesses_athlete_showpage_spec.rb
+++ spec/features/player_accesses_athlete_showpage_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 RSpec.feature 'user visits athlete showpage' do
   describe 'their own dashboard' do
     it 'sees individualized data with welcome' do
-      @athlete = create(:user, :complete, type: "Player")
+      @athlete = Player.create(username: 'bfav', password: 'password', first_name: 'Brett', last_name: 'Favret')
+      PlayerProfile.create(player: @athlete)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@athlete)
-      visit("/athletes/#{@athlete.id}")
-      expect(page).to have_content("Welcome Username")
+      visit("/player_profiles/#{@athlete.profile.id}")
+      expect(page).to have_content("Welcome Bfav")
       expect(page).to have_link("Edit Profile")
     end
   end
   describe 'someone elses dashboard' do
     it 'sees generic data and no welcome' do
       @athlete = create(:user, :complete, type: "Player")
-      athlete2 = create(:user, :complete, type: "Player", username: "Different Username")
+      athlete2 = Player.create(username: "Different Username", password: 'pass', first_name: 'fn', last_name: 'ln')
+      PlayerProfile.create(player: athlete2)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@athlete)
-      visit("/athletes/#{athlete2.id}")
+      visit("/player_profiles/#{athlete2.profile.id}")
 
       expect(page).to_not have_content("Welcome Different username")
       expect(page).to have_content(athlete2.first_name)
