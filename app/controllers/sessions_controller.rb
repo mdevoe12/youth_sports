6ffd1a2b358628f056c13808d7515e43e0abac14
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-09-07 23:04:32 -0600

    adds destroy to the list of skip_before_authorizing on SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
 
-  skip_before_action :authorize!, only: [:new, :create]
+  skip_before_action :authorize!, only: [:new, :create, :destroy]
 
 
   def new
