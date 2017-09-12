498199fe59d1189fa0f4dac7e6a36e80adf97251
Katie Keel <akatiekeel@gmail.com>
2017-09-12 15:34:06 -0600

    Wrote test for what user sees after clicking log in

---
+++ spec/features/login/user_clicks_log_in_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "User clicks the Log In button in the navbar and" do
+  scenario "sees a popup with options to log in with Facebook, Twitter, or default" do
+    
+  end
+end
