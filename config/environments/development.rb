bcd07151273faca7dddf944c5b422f857e850805
Brandon Randall <br.mhcexchange@gmail.com>
2017-09-14 13:13:51 -0600

    fixed untracked files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.after_initialize.bullet_enable = true
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
