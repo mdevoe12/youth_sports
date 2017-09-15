93108e9c03bfeb434c95514533371eef1bd18fed
Brandon Randall <br.mhcexchange@gmail.com>
2017-09-14 20:00:33 -0600

    modifies fav player spec

--- app/controllers/favorite_players_controller.rb
+++ app/controllers/favorite_players_controller.rb
@@ -6,9 +6,9 @@ class FavoritePlayersController < ApplicationController
 
   def create
     fav = params["favorite_player"]["screen_name"]
-    # @favorite_player = FavoritePlayer
-                    # .create(screen_name: fav, player_id: current_user.id)
-    @favorite_player = current_user.create_favorite_player(screen_name: fav)
+    @favorite_player = FavoritePlayer
+                    .create(screen_name: fav, player_id: current_user.id)
+    # @favorite_player = current_user.create_favorite_player(screen_name: fav)
     redirect_to player_profiles_path(current_user.id)
   end
 
