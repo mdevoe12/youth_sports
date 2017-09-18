074615a50bb2d61f408ea46dffb750bd40ba1390
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-18 08:35:20 -0600

    added sad path test

--- spec/features/coach/coach_can_upload_csv_spec.rb
+++ spec/features/coach/coach_can_upload_csv_spec.rb
@@ -32,12 +32,12 @@ RSpec.feature "A coach" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@coach)
   end
 
-  scenario "can see upload csv" do
+  scenario "can see upload successful csv" do
     visit "/dashboard"
 
     expect(page).to have_button("Upload CSV")
 
-    attach_file(:file, 'spec/features/upload_files/test_csv.csv')
+    attach_file(:file, 'spec/features/upload_files/good_data.csv')
     click_button "Upload CSV"
 
     stat = PlayerStat.last
@@ -48,5 +48,19 @@ RSpec.feature "A coach" do
     expect(stat.points).to eq(6)
     expect(stat.fouls).to eq(4)
     expect(stat.game_id).to eq(1)
+  end
+
+  scenario "uploads bad csv data" do
+    visit "/dashboard"
+
+    expect(page).to have_button("Upload CSV")
+
+    attach_file(:file, 'spec/features/upload_files/bad_data.csv')
+    click_button "Upload CSV"
+
+    stat = PlayerStat.last
+
+    expect(current_path).to eq(dashboard_index_path)
+    expect(page).to have_content("Items imported")
   end
 end
