5035d9d1ab7ffef7a09241a822507112b111ebe4
Katie Keel <akatiekeel@gmail.com>
2017-09-18 15:59:46 -0600

    Removed has_secure_password from admin

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.feature "admin can" do
   scenario "seed dashboard" do
     admin = Admin.create(first_name: "Bill",
-                         last_name: "Nye",
-                         password_digest: "123")
+                         last_name: "Nye")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
