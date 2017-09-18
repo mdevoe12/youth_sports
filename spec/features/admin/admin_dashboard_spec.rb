1a3b73bca38725e0cc9704768ddae5181cec5b20
Katie Keel <akatiekeel@gmail.com>
2017-09-18 16:01:51 -0600

    Fixed admin dashboard test

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "admin can" do
-  scenario "seed dashboard" do
-    admin = Admin.create(first_name: "Bill",
-                         last_name: "Nye")
+  scenario "see dashboard" do
+    admin = Admin.create(email: "bill@nye.com", password: "billnye")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
