dd87676b12832499457c2ac3a8684214f7cabede
Katie Keel <akatiekeel@gmail.com>
2017-09-18 15:57:39 -0600

    Removed skip before action authorize from controllers since Devise made it redundant

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170916223400) do
-=======
-ActiveRecord::Schema.define(version: 20170913211100) do
->>>>>>> development
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,15 +165,6 @@ ActiveRecord::Schema.define(version: 20170913211100) do
     t.datetime "updated_at", null: false
     t.index ["authentication_provider_id"], name: "index_user_authentications_on_authentication_provider_id"
     t.index ["user_id"], name: "index_user_authentications_on_user_id"
-  end
-
-  create_table "user_roles", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_user_roles_on_role_id"
-    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
