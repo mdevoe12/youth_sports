6bb2d990a7ba711ce1642fb96129dbaa2b86f08b
Katie Keel <akatiekeel@gmail.com>
2017-09-18 18:29:44 -0600

    Favorite player test passing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,13 @@ class DashboardController < ApplicationController
 
   def index
     redirect_to login_path if current_user.nil?
+
+    @tweet_info = build_twitter_service
   end
 
+  private
+
+  def build_twitter_service
+    @service ||= TwitterService.new(current_user)
+  end
 end
