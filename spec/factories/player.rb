fb058c1f9959139e5e0e1a0a203e6fcbc6461087
Katie Keel <akatiekeel@gmail.com>
2017-09-19 14:03:43 -0600

    Got tests to work

--- spec/factories/player.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :player do
-    first_name "firstname"
-    last_name "lastname"
-    username "usernameforplayer"
-    password "playerpassword"
-  end
-end
--- spec/factories/recruiter.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :recruiter do
-    first_name "Recruiter"
-    last_name "Rocks"
-    username "usernameforrecruiter"
-    password "passwordforrecruiter"
-  end
-end
