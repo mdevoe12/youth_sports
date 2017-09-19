afd51a541570df51ca5d31ea71a07152f0e15b91
Yohanan Assefa <nanahoy@gmail.com>
2017-09-19 12:29:41 -0600

    Run rake db:test:prepare to get the CI to pass

--- spec/features/recruiter/recruiter_can_add_and_delete_a_player_to_a_watchlist_spec.rb
+++ spec/features/recruiter/recruiter_can_add_and_delete_a_player_to_a_watchlist_spec.rb
@@ -15,9 +15,9 @@ feature "Recruiter can add and delete a player to a Watchlist" do
                          create(:team_player, player_id: @player.id, team_id: team.id)
                          create(:team_player, player_id: second_player.id, team_id: second_team.id )
                          create(:game_team, game_id: game.id, team_id: team.id)
-                         create(:game_team, game_id: game.id, team_id: second_team.id)
                          create(:game_team, game_id: second_game.id, team_id: team.id)
                          create(:game_team, game_id: second_game.id, team_id: second_game.id)
+                         create(:game_team, game_id: game.id, team_id: second_team.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@recruiter)
   end
