fa1cddc47ebf1cb1b48323b26717bf962e400d67
Katie Keel <akatiekeel@gmail.com>
2017-09-18 18:01:45 -0600

    Refactored player dash test to use factory girl

--- spec/features/player/player_dash_reveals_game_stats_spec.rb
+++ spec/features/player/player_dash_reveals_game_stats_spec.rb
@@ -2,20 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "registered player views their own dash" do
   describe "and sees a chart" do
-    before(:each) do
-      @player = Player.create(username: 'dvog', password: 'password', first_name: 'Brett', last_name: 'Favret')
-      PlayerProfile.create(player: @player)
-      @facility = create(:facility)
-      10.times do
-        game = create(:game, facility: @facility)
-        PlayerStat.create(created_at: "2017-09-05", :points => rand(0..10), :fouls => rand(0..5), player_profile_id: @player.profile.id, game_id: game.id)
-      end
-      game = create(:game, facility: @facility)
-      PlayerStat.create(created_at: "2016-09-05", :points => 11, :fouls => rand(0..5), player_profile_id: @player.profile.id, game_id: game.id)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@player)
-    end
     it "matches their last 10 games stats" do
-      visit("/player_profiles/#{@player.profile.id}")
+      player = create(:player, :with_profile, type: "Player")
+      player.teams << create(:team)
+      visit("/player_profiles/#{player.profile.id}")
       expect(page).to have_css("canvas#ath-profile-stats")
     end
   end
