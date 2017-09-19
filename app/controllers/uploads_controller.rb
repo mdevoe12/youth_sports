b2ae3828fca0f4f2e5806b87241be1177b1a8163
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-19 11:53:34 -0600

    created poro for csv importer, created csv importer job

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,7 +1,9 @@
+require 'csv'
 class UploadsController < ApplicationController
 
+
   def create
-    PlayerStat.import(params[:file])
+    CSVImporter.import_game_records(params[:file])
     redirect_to dashboard_index_path, notice: "Items imported"
   end
 
---
+++ app/models/csv_importer.rb
@@ -0,0 +1,18 @@
+class CSVImporter
+
+
+  def self.import_game_records(file)
+    ActiveRecord::Base.transaction do
+      CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
+        # return false unless Player.exists?(row[:id]) == true
+        stat = PlayerStat.new(player_profile_id: Player.find(row[:id]).profile.id,
+                                    game_id: row[:game_id],
+                                     points:  row[:points],
+                                      fouls:   row[:fouls])
+        stat.save!
+      end
+      # return true
+    end
+  end
+
+end
