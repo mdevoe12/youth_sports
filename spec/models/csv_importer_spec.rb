688e97a0195a8601273d1a9cdedde4f02fb68647
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-19 18:50:03 -0600

    working on csv_importer_spec.rb

--- spec/models/csv_importer_spec.rb
+++ spec/models/csv_importer_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe CSVImporter, type: :model do
   it "initializes with file" do
+    file = 'spec/features/upload_files/good_data.csv'
+
     importer = CSVImporter.new("sample")
 
     expect(importer.file).to eq("sample")
   end
 
+  it "imports a file with the run method" do
+
+  end
 end
