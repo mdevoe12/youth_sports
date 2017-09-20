ea9bf34920f2a5648ad243dee427b4f317710cac
Katie Keel <akatiekeel@gmail.com>
2017-09-20 11:26:04 -0600

    Removed through profile from recruiter should have many prospects

--- spec/models/recruiter_spec.rb
+++ spec/models/recruiter_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Recruiter, type: :model do
   it { should have_one(:profile) }
-  it { should have_many(:prospects), through: :profile}
+  it { should have_many(:prospects)}
 end
