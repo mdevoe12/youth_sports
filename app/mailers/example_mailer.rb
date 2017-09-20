5c45c7f46b9dba0325d06abdc75c7c7723ed43a4
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-20 10:34:48 -0600

    test added for sendemailjob

--- app/mailers/example_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ExampleMailer < ActionMailer::Base
-  default from: ENV['gmail_username']
-
-  def sample_email(file)
-    attachments["#{file.original_filename}"] = file.tempfile.read
-    mail(to: "matthew.devoe@gmail.com", subject: 'Sample Email')
-  end
-end
--- spec/jobs/send_email_job_spec.rb
+++ spec/jobs/send_email_job_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SendEmailJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "sends an email" do
+    ActiveJob::Base.queue_adapter = :test
+
+    expect {SendEmailJob.perform_later
+    }.to have_enqueued_job
+  end
 end
