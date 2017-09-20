91217eb9e6a50adeb7a4516f5a95594da781183e
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-20 09:48:32 -0600

    test for CSV importer job passes

--- spec/jobs/csv_importer_job_spec.rb
+++ spec/jobs/csv_importer_job_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CsvImporterJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "uploads a file" do
+    ActiveJob::Base.queue_adapter = :test
+
+    expect {CsvImporterJob.perform_later('file')
+    }.to have_enqueued_job
+  end
 end
